e9ea87950f817e822944ebac961cc3dfe7853f46
mauro alonso rey <mrey@lambda16.aulas.gsyc.es>
2015-02-10 11:43:02 +0100

    Calculadora using operator changes for pep8

--- calculadora.py
+++ calculadora.py
@@ -3,7 +3,7 @@
 
 import sys
 import os
-import operator 
+import operator
 
 operations = {
     'sumar': {'func': operator.add, 'char': '+'},
@@ -11,23 +11,21 @@ operations = {
     'dividir': {'func': operator.div, 'char': '/'},
     'restar': {'func': operator.sub, 'char': '-'}
 }
-inputs = sys.argv;
-_operator=inputs[1]
-operando1=inputs[2]
-operando2=inputs[3]
+inputs = sys.argv
+_operator = inputs[1]
+operando1 = inputs[2]
+operando2 = inputs[3]
 
 if len(inputs) != 4:
     print
     sys.exit("Usage: $ python calculadora.py operation operando1 operando2")
 
 try:
-    result=operations[_operator]['func'](int(operando1), int(operando2))
-    
-    print operando1, operations[_operator]['char'], operando2, '=' , result
-    
+    result = operations[_operator]['func'](int(operando1), int(operando2))
+    print operando1, operations[_operator]['char'], operando2, '=', result
 except KeyError:
-    print "Error operation: " + _operator 
+    print "Error operation: " + _operator
 except ValueError:
-    print "Error Value : Use integer numbers"  
+    print "Error Value : Use integer numbers"
 except ZeroDivisionError:
     print "Imposible dividir por cero"
